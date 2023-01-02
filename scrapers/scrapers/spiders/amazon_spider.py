db53411398fbd74397ddb67ef3aafeda910e9390
Andy Wu <andywurocks123@gmail.com>
2023-01-01 20:33:20 -0700

    added link and custom searching to amazon spider

--- scrapers/scrapers/spiders/amazon_spider.py
+++ scrapers/scrapers/spiders/amazon_spider.py
@@ -1,16 +1,24 @@
 import scrapy
 
 
-class CardsSpider(scrapy.Spider):
-    name = "cards"
-    start_urls = [
-        'https://www.amazon.com/s?k=turbotax+2022+deluxe&crid=5IR4UZZPJ0N4&sprefix=%2Caps%2C163&ref=nb_sb_ss_sx-trend-t-all-d_2_0'
-    ]
+class AmazonSpider(scrapy.Spider):
+    name = "amazon"
+
+    def __init__(self, search_phrase):
+        filtered = search_phrase.replace('+', '%2B')
+        words = filtered.split(' ')
+        search_string = '+'.join(words)
+        self.start_url = f'https://www.amazon.com/s?k={search_string}'
+        print('start url', self.start_url)
+
+    def start_requests(self):
+        yield scrapy.Request(url=self.start_url, callback=self.parse)
 
     def parse(self, response):
-        for card in response.css('div.s-card-container'):
+        for card in response.css('div.s-card-container.puis'):
             yield {
                 'image': card.css('img.s-image').attrib['src'],
+                'link': 'https://www.amazon.com' + card.css('a.a-text-normal').attrib['href'],
                 'title': card.css('span.a-size-medium::text').get(),
                 'stars': card.css('span.a-icon-alt::text').get(),
                 'price': card.css('span.a-price-whole::text').get(),
